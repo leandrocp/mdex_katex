764f7042ee13850d3256b7cb86357c6c38093644
Leandro Pereira <leandro@leandro.io>
2026-03-10 12:28:48 -0400

    v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.2.0 - 2026-03-10
+
+### Added
+- Render inline math (@nefty)
+
 ## 0.1.1 - 2026-01-09
 
 ### Changed
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MDExKatex.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leandrocp/mdex_katex"
-  @version "0.1.1"
+  @version "0.2.0"
 
   def project do
     [
